43e5d6bd8330095ce7da9a086af6ec3cfe5cffb3
Subas Rai <srai@totalwine.com>
2022-02-27 18:11:46 -0500

    APITest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {describe, expect, test} from ‘@testing-library/jest-dom’;
+
 
 const App = () => {
+    const [data, setdata] = useState({title : 'nodata yet'})
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data1 => {
+            const {title,id,body} = data1[0]
+            expect(title).toBe('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+            //if (id===1) {
+                //console.log("id matches");
+            //}
+             //else {
+                 //console.log("id doesn't match");
+             //}
+            setdata({
+                ...data, title, id, body
+           });
+        })
+    })
     return <>
         
 
@@ -8,7 +28,9 @@ const App = () => {
         
         
         <div style={{ padding: "50px 10px", border: "1px solid red", backgroundColor: "green" }}>
-            Good morning
+            <p>{data.id}</p>
+            <p>{data.title}</p>
+            <p>{data.body}</p>
         </div>
         
         
